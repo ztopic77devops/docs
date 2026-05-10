ecae04e9ecdfb58b0d2477b029fd8884443be27b
ztopic <zejlko.topic.sbt@gmail.com>
2026-05-10 12:32:14 +0200

    add mikrotik scripts

---
+++ mikrotik/set_pc_ip.sh
@@ -0,0 +1,53 @@
+#
+# this mikrotik script will find new PC connected to mikrotik
+# and will set address to 192.168.x.10 where x is location number took from local ip range
+# example: if location 007 have ip range 192.168.7.0/24 this script will will set PC ip to 192.168.7.11
+# note: that pc hostname must be <location_number-001> for example 007-001
+# dont forget to create c
+#
+
+:local myemail "email@domain.com";
+:local done false;
+:local ipaddress "$[/ip address get [ find where interface="ether5"] address ]";
+:local shopcode [:pick $ipaddress 7 [:find $ipaddress ".1/24"]];
+:local activeaddress "";
+:local shopid [:pick "$[/system identity get name]" 14 17];
+
+
+:do {
+
+	:local pccount 0
+
+	:foreach j in=[/ip dhcp-server lease find dynamic host-name=(""$shopid."-001") last-seen<15m] do={
+		:set pccount ($pccount+1)
+		:log info ("new pc found: " . [/ip dhcp-server lease get $j active-mac-address]);
+		:set $activeaddress [/ip dhcp-server lease get $j active-address];
+	}
+
+	:if ($pccount = 0) do={
+		:log info "there is no new pc on the network"
+	}
+
+	:if ($pccount > 1) do={
+		:log error "more then one pc found in DHCP leases"
+	}
+
+	:if ($pccount = 1) do={
+		/ip dhcp-server lease remove [find address=("192.168.".$shopcode.".11")];
+		:foreach j in=[/ip dhcp-server lease find dynamic host-name=(""$shopid."-001") last-seen<15m] do={
+			/ip dhcp-server lease make-static $j;
+			/ip dhcp-server lease set address=("192.168.".$shopcode.".11") comment="OTC" $j;
+		}
+
+	:set done true;
+	}
+
+}
+
+if ($done = true) do={
+	/tool e-mail\ 
+		send to=$myemail\
+		body=("new ps is connected on mikrotik $[/system identity get name] and have ip ("192.168.".$shopcode.".11"). for activation restart pc.")\
+		subject="$[/system identity get name] -- new pc connected"
+
+}
---
+++ mikrotik/update-config.sh
@@ -0,0 +1,55 @@
+#
+# this mikrotik script will check remote http server
+# if file srvVRS.txt on remote server is different from locVRS.txt on mikrotik
+# file config will be pulled from server to mikrotik, mikrotik CLI commands from config file will be applied
+# and locVRS.txt will be updated with srvVRS.txt content
+# 
+
+:local error false
+
+:if ([:len [file find name="locVRS.txt"]]!=1) do={
+	/file print file="locVRS.txt"
+}
+
+:do {
+	/tool fetch url="http://<server_ip_address>/mikrotik/srvVRS.txt" mode=http
+		} on-error={
+			:set error true
+			:log error "error while fetching srvVRS file"
+}
+
+:if ($error = false) do={
+	{delay 1};
+	:local locVRS [/file get locVRS.txt contents]
+	:local srvVRS [/file get srvVRS.txt contents]
+
+	:if ($locVRS != $srvVRS) do={
+		:do {
+			/tool fetch url="http://<server_ip_address>/mikrotik/config" mode=http
+		} on-error={
+			:set error true
+			:log error "error while fetching config file"
+		}
+		
+		:if ($error = false) do={
+			:do {
+				{delay 3};
+				/import file-name="config"
+			} on-error={
+				:set error true
+				:log error "error while importing config file"
+			}
+			
+			:if ($error = false) do={
+				/file set contents=$srvVRS locVRS.txt
+				{delay 5};
+				/file remove config
+			}
+		}
+	} else={
+		:log info "srvVRS same as locVRS"
+		
+	}
+}
+
+/file remove srvVRS.txt
