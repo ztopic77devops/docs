45ce9731b8521f943686c5d337dfd9a1fcdf264d
ztopic <zejlko.topic.sbt@gmail.com>
2026-05-11 20:32:24 +0200

    add services and autolock.ah

--- linux/autolock_desktop.sh
+++ linux/autolock_desktop.sh
@@ -1,3 +1,7 @@
+## this script will keep your laptop unlocked on your local network and lock it after 2 minutes on every other network
+
+# create a script with content:
+
 #!/bin/bash
 
 varCurrentIP=$(ifconfig)
@@ -14,3 +18,51 @@ else
     gsettings set org.cinnamon.desktop.session idle-delay 0
     echo "idle-delay disabled"
 fi
+
+
+##############################################################
+
+# create systemd service
+
+mkdir -p ~/.config/systemd/user
+
+vim ~/.config/systemd/user/autolock.service
+
+# add
+
+[Unit]
+Description=Autolock Script
+
+[Service]
+Type=simple
+ExecStart=/home/<user>/<path-to>/autolock_desktop.sh
+Restart=on-failure
+
+[Install]
+WantedBy=default.target
+
+##############################################################
+
+# make script executable
+
+chmod +x /home/<user>/<path-to>/autolock_desktop.sh
+
+# reload systemd
+
+systemctl --user daemon-reload
+
+# enable at login
+
+systemctl --user enable autolock.service
+
+# start service
+
+systemctl --user start autolock.service
+
+# check status
+
+systemctl --user status autolock.service
+
+# logs
+
+journalctl --user -u autolock.service -f
